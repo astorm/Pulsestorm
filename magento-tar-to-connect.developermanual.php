3c3832e45e8ccbf656284950417a5a85464228c2
Alan Storm <astorm@alanstorm.com>
2012-12-03 12:51:15 -0800

    Build for Developer Manual -- also, defaults to enabled.

---
+++ magento-tar-to-connect.developermanual.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+'base_dir'               => '/fakehome/Documents/github/Pulsestorm/var/build',
+'archive_files'          => 'Developer_Manual.tar',
+'extension_name'         => 'Developer_Manual',
+'extension_version'      => '1.1.1',
+'path_output'            => '/fakehome/Pulsestorm/var/build-connect',
+
+'stability'              => 'stable',
+'license'                => 'MIT',
+'channel'                => 'community',
+'summary'                => 'Provides navigation shortcuts for the admin console\'s navigation and gloal search',
+'description'            => 'This extension provides Magento admin console users with an "application launcher". This application launcher provides instant access to the admin console\'s navigation, every system configuration search section, as well as the Magento global search.  The Pulse Storm launcher is a free, open source, must have extension for anyone working with Magento. ',
+'notes'                  => 'Typo fixes, properly aborts ajax requests.',
+
+'author_name'            => 'Alan Storm',
+'author_user'            => 'alanstorm',
+'author_email'           => 'foo@example.com',
+'php_min'                => '5.2.0',
+'php_max'                => '6.0.0',
+'skip_version_compare'   => false,
+);
