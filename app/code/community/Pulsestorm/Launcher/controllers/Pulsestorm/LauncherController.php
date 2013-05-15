51368818d9422f751a476176f46d851ead43f7dc
Alan Storm <astorm@alanstorm.com>
2013-05-14 17:18:20 -0700

    Left testing publicActions in there.  Oops.

--- app/code/community/Pulsestorm/Launcher/controllers/Pulsestorm/LauncherController.php
+++ app/code/community/Pulsestorm/Launcher/controllers/Pulsestorm/LauncherController.php
@@ -2,12 +2,6 @@
 require_once 'Mage/Adminhtml/controllers/IndexController.php';
 class Pulsestorm_Launcher_Pulsestorm_LauncherController extends Mage_Adminhtml_IndexController
 {
-    protected $_publicActions=array('index','globalSearch');
-    public function indexAction()
-    {
-        var_dump(__METHOD__);
-    }
-    
     public function globalSearchAction()
     {
         //end session to avoid locking problems
